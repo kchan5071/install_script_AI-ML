58ba7299f3a41e841a31741265b8fceebb095397
kchan5071 <kchan5071@sdsu.edu>
2025-01-23 17:36:13 -0800

    tested

--- install.sh
+++ install.sh
@@ -2,8 +2,17 @@
 
 # Install the required packages
 sudo apt-get update
-sudo apt-get install -y python3.8
-sudo apt-get install -y python3-pip python3-venv
+sudo apt-get install -y python3 python3-pip python3-venv
+
+mkdir -p AI-sandbox
+mv requirements.txt AI-sandbox
+cd AI-sandbox
+
+# Create a virtual environment
+python3 -m venv AI-sandbox-env
+
+# Activate the virtual environment
+source AI-sandbox-env/bin/activate
 
 # use pip3 to install the required packages
 pip3 install -r requirements.txt
