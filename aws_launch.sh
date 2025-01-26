af0115a5e47f3fcd25bf4c0ffb87f2974f43e204
Kai Chan <145510134+kchan5071@users.noreply.github.com>
2025-01-25 20:53:43 -0800

    Update aws_launch.sh

--- aws_launch.sh
+++ aws_launch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 mkdir /var/log/startup
-echo "startup started" >> /var/log/startup/log.txt
+echo "startup started\n" >> /var/log/startup/log.txt
 cd ~
 git clone https://github.com/kchan5071/install_script_AI-ML.git
-echo "initial clone complete" >> /var/log/startup/log.txt
+echo "initial clone complete\n" >> /var/log/startup/log.txt
 cd install_script_AI-ML
 chmod +x install.sh
-echo "changed permissions for install" >> /var/log/startup/log.txt
+echo "changed permissions for install\n" >> /var/log/startup/log.txt
 ./install.sh
-echo "startup finished" >> /var/log/startup/log.txt
+echo "startup finished\n" >> /var/log/startup/log.txt
